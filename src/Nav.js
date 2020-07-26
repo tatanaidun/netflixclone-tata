04807f05274d772e111f605631fb5d77bef7bc85
Tata Naidu Nukala <tatanaidunukala@gmail.com>
2020-07-26 23:46:19 +0530

    netflixclone: completed commit
    Signed-off-by: Tata Naidu Nukala <tatanaidunukala@gmail.com>

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      {/* 
+      <img 
+          className="nav_avatar"
+          src="https://pbs.twimg.com/profile_images/12401199904115"
+      /> */}
+    </div>
+  );
+}
+
+export default Nav;
